8c2d3d1d3c6e9f56a40457c7263190528e826e24
Lázár Viktor <lazarv1982@gmail.com>
2016-11-30 22:39:17 +0100

    Bump version

--- lib/odataUri.d.ts
+++
@@ -1,2 +0,0 @@
-import * as Lexer from './lexer';
-export declare function odataUri(value: number[] | Uint8Array, index: number, metadataContext?: any): Lexer.Token;
--- lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function stringify(value: number[] | Uint8Array, index: number, next: number): string;
-export declare function is(value: number, compare: string): boolean;
-export declare function equals(value: number[] | Uint8Array, index: number, compare: string): number;
-export declare function required(value: number[] | Uint8Array, index: number, comparer: Function, min?: number, max?: number): number;
